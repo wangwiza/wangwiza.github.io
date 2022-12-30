7cdb4e4ff8011107389fe1a829f7e913d0235f6b
wangwiza <76924804+wangwiza@users.noreply.github.com>
2022-12-30 15:29:49 -0500

    fix(skill-lib): icon size

--- src/components/SkillLib.jsx
+++ src/components/SkillLib.jsx
@@ -1,11 +1,9 @@
 import * as React from 'react'
 import { 
-    Box,
     HStack,
     VStack,
     Text,
     Icon,
-
 } from '@chakra-ui/react'
 import {
     IoLogoJavascript,
---
+++ src/components/Works.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import {
+    SimpleGrid,
+
+} from '@chakra-ui/react'
+
+function Works(){
+    return (
+        <SimpleGrid>
+            
+        </SimpleGrid>
+    )
+}
