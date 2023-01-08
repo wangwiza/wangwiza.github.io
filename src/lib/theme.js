2072bbb687efe315addddde6fdd5f42dbc625260
wangwiza <76924804+wangwiza@users.noreply.github.com>
2023-01-07 19:33:51 -0500

    fix(theme): theme.js reset

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -4,45 +4,17 @@ import { mode } from '@chakra-ui/theme-tools'
 const styles = {
     global: props => ({
         body: {
+            // bg color for HTML body
             bg: mode('#f0e7db', '#202023')(props)
         }
     })
 }
 
-const components = {
-    Heading: {
-        variants: {
-            'section-title': {
-                textDecoration: 'underline',
-                fontSize: 20,
-                textUnderlineOffset: 6,
-                textDecorationColor: '#525252',
-                textDecorationThickness: 4,
-                marginTop: 3,
-                marginBottom: 4
-            }
-        }
-    },
-    Link: {
-        baseStyle: props => ({
-            color: mode('#3d7aed', '#ff63c3')(props),
-            textUnderlineOffset: 3
-        })
-    }
-}
-
-const fonts = {
-    heading: "'Montserrat', sans serif"
-}
-
-const colors = {
-    grassTeal: '#88ccca'
-}
-
 const config = {
+    // Default bg color mode
     initialColorMode: 'dark',
     useSystemColorMode: true
 }
 
-const theme = extendTheme({ config, styles, components, fonts, colors })
+const theme = extendTheme({ config, styles })
 export default theme
